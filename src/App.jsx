76a9176ec55117ac6c863cc30049f0131cc2612e
Suneel Kumar <shrikantsinghkashmeawahi@gmail.com>
2024-06-02 04:13:18 +0530

    add asignment day 4

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import  "./App.css";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Section from "./Section";
+import Footer from "./Footer";
+import Reviews from "./Reviews";
+
+function App(){
+  return(
+    <> 
+     <Navbar />
+     <Hero />
+     <Section />
+     <Footer/>
+     <Reviews />
+     
+    </>
+  )
+}
+
+export default App;
